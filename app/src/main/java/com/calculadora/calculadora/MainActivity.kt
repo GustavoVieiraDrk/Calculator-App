2d60106f633eb9cbdc3244195e41f1f163a49610
Gustavo_Vs <gustavovieira@vago.online>
2021-10-27 08:32:55 -0300

    Caculator app with diferents layouts

---
+++ app/src/main/java/com/calculadora/calculadora/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.calculadora.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun printDisplay (string: String, ClearData: Boolean) {
+
+        }
+
+    fun actionClear(view: View) {
+
+    }
+
+}
